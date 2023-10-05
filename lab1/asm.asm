ab07320b8834345741f651f81de721fc3f0444f7
Fedor <its_sun_exploding@mail.ru>
2023-10-05 21:25:16 +0300

    Initial branch commit

--- lab1/asm.asm
+++ lab1/asm.asm
@@ -1,7 +1,20 @@
-section .data
-	x db 4
-section .text
-global asm_func
-asm_func:
-	mov  eax, 4
-	inc  eax
+section	.text
+   global start ; должно быть объявлено для линкера (gcc)
+	
+start:	         ; сообщаем линкеру точку входа
+   mov	edx,len  ; длина сообщения
+   mov	ecx,msg  ; сообщение для вывода на экран
+   mov	ebx,1    ; файловый дескриптор (stdout)
+   mov	eax,4    ; номер системного вызова (sys_write)
+   int	0x80     ; вызов ядра
+	
+   mov	edx,9    ; длина сообщения
+   mov	ecx,s2   ; сообщение для написания
+   mov	ebx,1    ; файловый дескриптор (stdout)
+   mov	eax,4    ; номер системного вызова (sys_write)
+   int	0x80     ; вызов ядра
+	
+section	.data
+msg db 'Displaying 9 stars',0xa ; наше сообщение
+len equ $ - msg  ; длина нашего сообщения 
+s2 times 9 db '*'
--- lab1/c.c
+++ lab1/c.c
@@ -1,31 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-extern void asm_func(void);
+extern void start(void);
 
-int 
-main(int argc, char *argv[]) {
-	// asm_func();
-	// (55-b+1*a)/(-88/c+1)
-	signed int a, b, c;
-	printf("%d\n", sizeof(int));
-	printf("Range of signed int: 0-65535 \n");
-	printf("a = ");
-	scanf("%d", &a);
+int main(){
 
-	printf("b = ");
-	scanf("%d", &b);
-
-	printf("c = ");
-	scanf("%d", &c);
-
-	// Signed Int 16-bit
-	signed int num, den, res;
-	num = 55 - b + a;
-	den = (-88 / c) + 1;
-	res = (num / den);
-	printf("%d\n", num);
-	printf("%d\n", den);
-	printf("%d\n", res);
-
+	start();
+	printf("LOL!\n");
+	start();
+	printf("Hello!");
+	start();
 	return 0;
 }
