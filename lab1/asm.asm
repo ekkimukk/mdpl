3d609d250c9abc3ad33dcacf287fc4e144180493
egErTolva <songbirdwhosing@gmail.com>
2023-10-14 12:48:35 +0300

    okey signed int done

--- lab1/asm.asm
+++ lab1/asm.asm
@@ -1,38 +1,40 @@
 section .data
-	extern sia , sib , sic , siNum , siDen , siRes
-	extern usia , usib , usic , usiNum , usiDen , usiRes
-	extern sca , scb , scc , scNum , scDen , scRes
-	extern usca , uscb , uscc , uscNum , uscDen , uscRes
+	extern Num  , Den  , Res
+	extern sia  , sib  , sic
+	extern usia , usib , usic
+	extern sca  , scb  , scc
+	extern usca , uscb , uscc
 
 section .text
 	global asm_signed_int
 	global asm_unsigned_int
 	global asm_signed_char
-	global asm_unsigned_char
+	global asm_unsigned_char 
+
 asm_signed_int:
-;	; numerator
-;	mov    bx,      [sib]    ; bx = b
-;	mov    ax,      55	     ; ax = 55
-;	sub    ax,      bx       ; ax = 55 - b
-;	mov    bx,      [sia]      ; bx = a
-;	add    ax,      bx       ; ax = 55 - b + a
-;	mov    [siNum], ax       ; siNum = 55 - b + a
-;
-;	; denominator
-;	mov    ax,      -88      ; ax = -88
-;	cwd						 ; ax:dx = -88
-;	mov    bx,      [sic]    ; bx = c
-;	idiv   bx                ; ax = -88 / c
-;	inc    ax                ; ax = -88 / c + 1
-;	mov    [siDen], ax       ; siDen = 55 - b + a
-;
-;	; result
-;	mov    ax,      [siNum]  ; ax = 55 - b + a
-;	cwd						 ; ax:dx = 55 - b + a
-;	mov    bx,      [siDen]  ; bx = -88 / c + 1
-;	idiv   bx				 ; ax = num / den
-;	mov    [siRes], ax       ; siRes = num / den
-ret
+	; numerator
+	mov    bx,      [sib]    ; bx = b
+	mov    ax,      55	     ; ax = 55
+	sub    ax,      bx       ; ax = 55 - b
+	mov    bx,      [sia]    ; bx = a
+	add    ax,      bx       ; ax = 55 - b + a
+	mov    [Num],   ax       ; Num = 55 - b + a
+
+    ; denominator
+	mov    ax,      -88      ; ax = -88
+	cwd						 ; ax:dx = -88
+	mov    bx,      [sic]    ; bx = c
+	idiv   bx                ; ax = -88 / c
+	inc    ax                ; ax = -88 / c + 1
+	mov    [Den],   ax       ; Den = 55 / c + 1
+
+	; result
+	mov    ax,      [Num]    ; ax = 55 - b + a
+	cwd						 ; ax:dx = 55 - b + a
+	mov    bx,      [Den]    ; bx = -88 / c + 1
+	idiv   bx				 ; ax = num / den
+	mov    [Res],   ax       ; Res = ax = Num / Den
+
 
 asm_unsigned_int:
 	; numerator
@@ -42,16 +44,19 @@ ret
 
 asm_signed_char:
 	; numerator
-	mov    bx,      [scb]    ; bx = b
+	mov  al,      [scb]    ; al = b
+	cbw                      ; ax = b
+	mov    bx,      ax       ; bx = b 
 	mov    ax,      55       ; ax = 55
 	sub    ax,      bx       ; ax = 55 - b
-	mov    bx,      [sca]    ; bx = a
-	add    ax,      bx       ; ax = 55 - b + a
-	mov    [scNum], ax       ; scNum = ax
+	mov    bx,      ax       ; bx = 55 - b
+	mov  al,      [sca]    ; al = a
+	cbw						 ; ax = a
+	add    bx,      ax       ; ax = 55 - b + a
+	mov    [Num],   ax       ; Num = ax
 
 	; denominator
 
 	; result
-
-ret 
+ret
 
--- lab1/c.c
+++ lab1/c.c
@@ -6,60 +6,62 @@ extern void asm_unsigned_int(void);
 extern void asm_signed_char(void);
 extern void asm_unsigned_char(void);
 
-int16_t sia, sib, sic, siNum, siDen, siRes; // signed_int
-uint16_t usia, usib, usic, usiNum, usiDen, usiRes; // unsigned_int
-int8_t sca, scb, scc, scNum, scDen, scRes; // signed_char
-int8_t usca, uscb, uscc, uscNum, uscDen, uscRes; // unsigned_char
+int Num, Den, Res;
+signed int sNum, sDen, sRes;
+int16_t sia, sib, sic; // signed_int
+uint16_t usia, usib, usic; // unsigned_int
+int8_t sca, scb, scc; // signed_char
+int8_t usca, uscb, uscc; // unsigned_char
 
 
 void siC(int16_t sia, int16_t sib, int16_t sic) {
-	siNum = 0; siDen = 0; siRes = 0;
-	siNum = 55 - sib + sia;
-	siDen = (-88 / sic) + 1;
-	siRes = (siNum / siDen);
-	printf("%d\n", siNum);
-	printf("%d\n", siDen);
-	printf("%d\n", siRes);
+	Num = 0; Den = 0; Res = 0;
+	Num = 55 - sib + sia;
+	Den = (-88 / sic) + 1;
+	Res = (Num / Den);
+	printf("%d\n", Num);
+	printf("%d\n", Den);
+	printf("%d\n", Res);
 }
 
 void siASM(int16_t sia, int16_t sib, int16_t sic) {
 	sia = sia; sib = sib; sic = sic;
-	siNum = 0; siDen = 0; siRes = 0;
+	Num = 0; Den = 0; Res = 0;
 	asm_signed_int();
 }
 
 
 void usiC(uint16_t usia, uint16_t usib, uint16_t usic) {
-	usiNum = 0; usiDen = 0; usiRes = 0;
-	usiNum = 55 - usib + usia;
-	usiDen = (-88 / usic) + 1;
-	usiRes = (usiNum / usiDen);
-	printf("%u\n", usiNum);
-	printf("%u\n", usiDen);
-	printf("%u\n", usiRes);
+	Num = 0; Den = 0; Res = 0;
+	Num = 55 - usib + usia;
+	Den = (-88 / usic) + 1;
+	Res = (Num / Den);
+	printf("%d\n", Num);
+	printf("%d\n", Den);
+	printf("%d\n", Res);
 }
 
 void usiASM(uint16_t usia, uint16_t usib, uint16_t usic) {
 	usia = usia; usib = usib; usic = usic;
-	usiNum = 0; usiDen = 0; usiRes = 0;
+	Num = 0; Den = 0; Res = 0;
 	asm_unsigned_int();
 }
 
 
 void scC(int8_t sca, int8_t scb, int8_t scc) {
-	scNum = 0; scDen = 0; scRes = 0;
-	scNum = 55 - scb + sca;
-	scDen = (-88 / scc) + 1;
-	scRes = (scNum / scDen);
-	printf("%d\n", scNum);
-	printf("%d\n", scDen);
-	printf("%d\n", scRes);
+	Num = 0; Den = 0; Res = 0;
+	Num = 55 - scb + sca;
+	Den = (-88 / scc) + 1;
+	Res = (Num / Den);
+	printf("%d\n", Num);
+	printf("%d\n", Den);
+	printf("%d\n", Res);
 }
 
 void scASM(int8_t sia, int8_t sib, int8_t sic) {
 	sca = sca; scb = scb; scc = scc;
-	scNum = 0; scDen = 0; scRes = 0;
-	asm_signed_int();
+	Num = 0; Den = 0; Res = 0;
+	asm_signed_char();
 }
 
 
@@ -67,9 +69,8 @@ void scASM(int8_t sia, int8_t sib, int8_t sic) {
 int 
 main(int argc, char *argv[]) {
 	// (55-b+1*a)/(-88/c+1) 
-	printf("P.S. a=0  b=10  c=-44  :)\n");
-	/*
-	// Signed Int
+	
+	// Signed Int ()
 	printf("sia = ");
 	scanf("%d", &sia);
 
@@ -80,10 +81,10 @@ main(int argc, char *argv[]) {
 	scanf("%d", &sic);
 
 	siASM(sia, sib, sic);
-	siC(sia, sib, sic);
+	//siC(sia, sib, sic);
 	// ---------------
-	
-	// Unsigned Int
+	/*
+	// Unsigned Int (0<->65535)
 	printf("usia = ");
 	scanf("%u", &usia);
 
@@ -96,24 +97,21 @@ main(int argc, char *argv[]) {
 	usiASM(usia, usib, usic);
 	usiC(usia, usib, usic);
 	// ---------------
-	*/
-	// Signed Char
+	
+	// Signed Char (-128<->127)
 	printf("sca = ");
 	scanf("%d", &sca);
-	printf("%d\n", sca);
 
 	printf("scb = ");
 	scanf("%d", &scb);
-	printf("%d\n", scb);
 
 	printf("scc = ");
 	scanf("%d", &scc);
-	printf("%d\n", scc);
 
 	scASM(sca, scb, scc);
 	//scC(sca, scb, scc);
 	// ---------------
-
+	*/
 
 	return 0;
 }
